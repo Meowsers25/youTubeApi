82b066397a65df62618d99daf9ccf0f1d2cdc919
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-08-01 04:17:43 +0000

    Initial commit

---
+++ keys.js
@@ -0,0 +1,1 @@
+var youTubeApiKey = "AIzaSyCB6RKJY7fa8d-uO4go21kJOWhXjj2Voa0";
---
+++ script.js
@@ -0,0 +1,31 @@
+/* global youTubeApiKey $*/
+$(document).ready(function() {
+
+    $("#searchYouTube").submit(function(event) {
+        event.preventDefault(); //tell \s to do this instead
+        var query = $('#query').val();
+        var url = "https://www.googleapis.com/youtube/v3/search";
+        var data = {
+            key: youTubeApiKey,
+            part: "snippet",
+            q: query
+        };
+         $.ajax({
+            url: url,
+            data: data,
+            type: "GET",
+            success: function(response) {
+                //console.log(response);
+                var items = response.items;
+                var html = "<ul class='list-group'>";
+                $.each(items, function(index, item) {
+                   //var item = items[index] 
+                  console.log(item.snippet.title);
+                  html += "<li class='list-group-item'>" + item.snippet.title + "</li>"
+                });
+                html += "</ul>";
+                $("#results").html(html);
+            }
+        });
+    });
+});
